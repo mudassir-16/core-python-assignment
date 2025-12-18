7997ac3185ff40da4655244511b06fceb7b0a0c8
Mohammed Mudassir <mohammadmudassir1604@gmail.com>
2025-12-18 16:31:29 +0530

    Update taxi_fare.py

--- taxi_fare.py
+++ taxi_fare.py
@@ -1,8 +1,8 @@
 def calculate_fare(distance):
-    return 50 + distance * 10
+    return 50 + (distance * 10)
 
 
-trips = list(map(int, input("Enter trip distances separated by space: ").split()))
+trips = [5, 10, 3]
 total = 0
 
 for i, d in enumerate(trips, 1):
